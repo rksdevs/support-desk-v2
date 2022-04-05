911eddefe32c4056654acf41c9d7b17ba686c830
rksdev <mailto.dev.rakesh@gmail.com>
2022-04-06 00:52:11 +0530

    changes to server file

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   // FIX: below code fixes app crashing on refresh in deployment
-  app.get("*", (req, res) => {
+  app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
   });
 } else {
